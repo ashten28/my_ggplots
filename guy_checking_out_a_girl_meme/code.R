b88895fc3240f4d8c0946adb7835e58a89ce2d5c
ashten28 <ashten28@gmail.com>
2020-04-24 01:12:15 +0800

    add session

--- guy_checking_out_a_girl_meme/code.R
+++ guy_checking_out_a_girl_meme/code.R
@@ -78,7 +78,9 @@ p2 <-
     legend.position = "none"
   )
 
+# using patchwork to combine main plot and custom legend
 p <- p1 + p2
+p
 
 # save plot
 ggsave(filename = "guy_checking_out_a_girl_meme/plot.png", plot = p, width = 16, height = 9)
@@ -86,15 +88,26 @@ ggsave(filename = "guy_checking_out_a_girl_meme/plot.png", plot = p, width = 16,
 
 
 
-## inital geom_tile
-# ggplot(data) +
-#   geom_tile(
-#     mapping = aes(x = x, y = y, fill = paint)
-#   ) +
-#   coord_equal() +
-#   scale_fill_manual(
-#     values = c("#e4a095", "#24211a", "#0b59b3", "#593326", "#93a9b6", "#fa0107")
-#   ) +
-#   theme_void()
+# R version 3.6.3 (2020-02-29)
+# Platform: x86_64-w64-mingw32/x64 (64-bit)
+# Running under: Windows 7 x64 (build 7601) Service Pack 1
+# 
+# Matrix products: default
+# 
+# locale:
+#   [1] LC_COLLATE=English_Australia.1252  LC_CTYPE=English_Australia.1252    LC_MONETARY=English_Australia.1252 LC_NUMERIC=C                      
+# [5] LC_TIME=English_Australia.1252    
+# 
+# attached base packages:
+#   [1] stats     graphics  grDevices utils     datasets  methods   base     
+# 
+# other attached packages:
+#   [1] patchwork_1.0.0 ggplot2_3.3.0   forcats_0.5.0   dplyr_0.8.5     readr_1.3.1    
+# 
+# loaded via a namespace (and not attached):
+#   [1] Rcpp_1.0.4.6     rstudioapi_0.11  magrittr_1.5     hms_0.5.3        tidyselect_1.0.0 munsell_0.5.0    colorspace_1.4-1 R6_2.4.1        
+# [9] rlang_0.4.5      fansi_0.4.1      tools_3.6.3      grid_3.6.3       packrat_0.5.0    gtable_0.3.0     cli_2.0.2        withr_2.2.0     
+# [17] ellipsis_0.3.0   digest_0.6.25    assertthat_0.2.1 tibble_3.0.0     lifecycle_0.2.0  crayon_1.3.4     farver_2.0.3     purrr_0.3.4     
+# [25] vctrs_0.2.4      glue_1.4.0       labeling_0.3     compiler_3.6.3   pillar_1.4.3     scales_1.1.0     pkgconfig_2.0.
 
 
